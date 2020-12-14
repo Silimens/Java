bbd7e3658d11baff4bda7d0bf99c12900f01f216
Silimens <53562744+Silimens@users.noreply.github.com>
2020-12-15 01:16:53 +0300

    Update RunnerSAX.java

--- src/RunnerSAX.java
+++ src/RunnerSAX.java
@@ -14,7 +14,7 @@ public class RunnerSAX {
     public static void main(String[] args) {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         factory.setNamespaceAware(true);
-        ArticleHandler handler = new ArticleHandler(); // Перемещиение по XML документу
+        ArticleHandler handler = new ArticleHandler(); 
         try {
             SAXParser parser = factory.newSAXParser();
             parser.parse(new InputSource(new URL(Constants.URL).openStream()), handler);
